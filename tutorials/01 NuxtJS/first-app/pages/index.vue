8f8538a1eaef3805e9a1c1009a14b68b31f00703
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-07-19 17:50:04 +0300

    start learning NuxtJS

---
+++ tutorials/01 NuxtJS/first-app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- <Tutorial/> -->
+  <div>
+    <h1>Hello world!</h1>
+    <NuxtLink to="/test">Go to test page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
