46374b281d04beaae1abf3d65dfc2132f2e00f02
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-08-05 14:36:28 +0300

    change id field to be UUID

--- backend/class/models.py
+++ backend/class/models.py
@@ -1,7 +1,12 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 class Class(models.Model):
+    class Meta:
+        verbose_name_plural = "Classes"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
 
     def __str__(self):
