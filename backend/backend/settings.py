39e57393b7131d75ce745102bce49dcbe80ea42f
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-08-05 13:42:35 +0300

    change DB from MariaDB to PostgreSQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
         "PASSWORD": env("DB_PASS"),
