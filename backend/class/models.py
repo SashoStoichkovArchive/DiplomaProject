996f84f46708d93118503c165c13fa98dc54c998
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-08-05 15:52:11 +0300

    add utils - model with UUID for id

--- backend/class/models.py
+++ backend/class/models.py
@@ -1,12 +1,11 @@
-import uuid
 from django.db import models
+from utils.models import UUIDModel
 
 # Create your models here.
-class Class(models.Model):
+class Class(UUIDModel):
     class Meta:
         verbose_name_plural = "classes"
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
 
     def __str__(self):
