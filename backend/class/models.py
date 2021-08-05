7c788723a338a28b65fc1e3bcc41c877941fdab1
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-08-05 14:41:25 +0300

    fix typo

--- backend/class/models.py
+++ backend/class/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Class(models.Model):
     class Meta:
-        verbose_name_plural = "Classes"
+        verbose_name_plural = "classes"
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
